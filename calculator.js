b7b951e814cccb6a40e06859b1719e7e71283b86
brandonricharda <brandonricharda@gmail.com>
2020-01-22 21:28:19 -0500

    Keyboard functionality updated.

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 //Initial Presets
 let status = {
-    displayObject : document.querySelector('#calculator-input'),
+    mainDisplay : document.querySelector('#calculator-input'),
     current : 'placeholder',
     runningTally : 0,
     firstNumberOfOperation : 0,
@@ -13,10 +13,10 @@ let status = {
     numToBackspace : '',
 }
 //Creates Number Buttons
-status.displayObject.textContent = 0;
+status.mainDisplay.textContent = 0;
 for (i = 0; i <= 11; i++) {
+    let calculatorNumbers = document.querySelector('#calculator-numbers');
     if (i <= 9) {
-        let calculatorNumbers = document.querySelector('#calculator-numbers');
         let numberButton = document.createElement('button');
         numberButton.setAttribute('id', 'button' + i);
         numberButton.textContent = i;
@@ -24,7 +24,6 @@ for (i = 0; i <= 11; i++) {
         numberButton.style.gridArea = 'button' + i;
         numberButton.addEventListener('click', () => numberInput(numberButton.textContent));
     } else if (i == 10) {
-        let calculatorNumbers = document.querySelector('#calculator-numbers');
         let decimalButton = document.createElement('button');
         decimalButton.setAttribute('id', 'buttonDecimal');
         decimalButton.textContent = ' \267 ';
@@ -33,7 +32,6 @@ for (i = 0; i <= 11; i++) {
         decimalButton.style.gridArea = 'buttonDecimal';
         decimalButton.addEventListener('click', () => numberInput('.'));
     } else if (i == 11) {
-        let calculatorNumbers = document.querySelector('#calculator-numbers');
         let backspaceButton = document.createElement('button');
         backspaceButton.setAttribute('id', 'buttonDecimal');
         backspaceButton.textContent = 'DEL';
@@ -42,7 +40,7 @@ for (i = 0; i <= 11; i++) {
         backspaceButton.addEventListener('click', () => backspace());
     }
 }
-//Creates Function Buttons
+//Creates Operator Buttons
 for (j = 0; j < 6; j++) {
     let calculatorOperations = document.querySelector('#calculator-operations');
     let operationButton = document.createElement('button');
@@ -73,23 +71,22 @@ for (j = 0; j < 6; j++) {
 }
 //Arithmetic Functions
 function add() {
-    status.displayObject.textContent = parseInt(status.firstNumberOfOperation) + parseInt(status.secondNumberOfOperation);
+    status.mainDisplay.textContent = parseInt(status.firstNumberOfOperation) + parseInt(status.secondNumberOfOperation);
     prepareNextRound();
 }
 function subtract() {
-    status.displayObject.textContent = status.firstNumberOfOperation - status.secondNumberOfOperation;
+    status.mainDisplay.textContent = status.firstNumberOfOperation - status.secondNumberOfOperation;
     prepareNextRound();
 }
 function multiply() {
-    status.displayObject.textContent = status.firstNumberOfOperation * status.secondNumberOfOperation;
-    status.result = status.displayObject.textContent;
+    status.mainDisplay.textContent = status.firstNumberOfOperation * status.secondNumberOfOperation;
     prepareNextRound();
 }
 function divide() {
     if (status.firstNumberOfOperation == 0 || status.secondNumberOfOperation == 0) {
-        status.displayObject.textContent = 'You sneaky bastard...';
+        status.mainDisplay.textContent = 'Error';
     } else {
-        status.displayObject.textContent = status.firstNumberOfOperation / status.secondNumberOfOperation;
+        status.mainDisplay.textContent = status.firstNumberOfOperation / status.secondNumberOfOperation;
     }
     prepareNextRound();
 }
@@ -115,36 +112,23 @@ function numberInput (number) {
     if (number != 0 || status.current != 'placeholder') {
         if (status.current == 'placeholder' && number != '.') {
             status.current = number;
-            status.displayObject.textContent = status.current;
+            status.mainDisplay.textContent = status.current;
             status.runningTally = status.current;
         } else if (number == '.' && status.decimalAlert == '') {
             status.decimalAlert = 'active';
             status.current = number;
-            status.displayObject.textContent = ('' + status.runningTally + status.current);
-            status.runningTally = status.displayObject.textContent;                
+            status.mainDisplay.textContent = ('' + status.runningTally + status.current);
+            status.runningTally = status.mainDisplay.textContent;                
         } else if (number != '.' && number != 'backspace') {
             status.current = number;
-            status.displayObject.textContent = ('' + status.runningTally + status.current);
-            status.runningTally = status.displayObject.textContent;
+            status.mainDisplay.textContent = ('' + status.runningTally + status.current);
+            status.runningTally = status.mainDisplay.textContent;
         }
     } else if (number == 0 && status.current == 'placeholder') {
         //Do nothing; user is trying to start a number with zero.
     } 
 }
-//Logic Functions
-function prepareNextRound() {
-    status.result = status.displayObject.textContent;
-    status.current = 'placeholder';
-    status.runningTally = status.result;
-}
-function afterOperationButtonClick() {
-    status.current = 'placeholder';
-    status.runningTally = 0;
-    status.loopAlert = '';
-    status.decimalAlert = '';
-}
 function clear() {
-    status.displayObject = document.querySelector('#calculator-input');
     status.current = 'placeholder';
     status.runningTally = 0;
     status.firstNumberOfOperation = 0;
@@ -153,24 +137,45 @@ function clear() {
     status.result = 0;
     status.numberOfRuns = 0;
     status.loopAlert = '';
-    status.displayObject.textContent = 0;
+    status.mainDisplay.textContent = 0;
     status.decimalAlert = '';
 }
 function backspace() {
-    status.numToBackspace = status.displayObject.textContent;
-    status.displayObject.textContent = status.numToBackspace.slice(0, -1);
-    status.runningTally = status.displayObject.textContent;
+    status.numToBackspace = status.mainDisplay.textContent;
+    status.mainDisplay.textContent = status.numToBackspace.slice(0, -1);
+    status.runningTally = status.mainDisplay.textContent;
 }
-window.onkeydown = function(event) {
-    console.log(event);
+document.onkeydown = function(event) {
     if (event.key == 'Backspace') {
         backspace();
     } else if (event.key < 10) {
         numberInput(event.key);
     } else if (event.key == 'Enter') {
         equals();
+        operationSelector('equals');
     } else if (event.key == 'x') {
-        this.operationSelector('multiply');
+        operationSelector('multiply');
+    } else if (event.key == '/') {
+        operationSelector('divide');
+    } else if (event.key == '.') {
+        numberInput('.');
+    } else if (event.key == 'Escape') {
+        clear();
+    } else if (event.key == '-') {
+        operationSelector('subtract');
+    } else if (event.key == '+') {
+        operationSelector('add');
     }
-    //numberInput(event.key);
+}
+//Logic Functions
+function prepareNextRound() {
+    status.result = status.mainDisplay.textContent;
+    status.current = 'placeholder';
+    status.runningTally = status.result;
+}
+function afterOperationButtonClick() {
+    status.current = 'placeholder';
+    status.runningTally = 0;
+    status.loopAlert = '';
+    status.decimalAlert = '';
 }
